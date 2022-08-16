34519dde61292b5217eb8214efe6300849da069e
Dirac Murairi <dmurairimukongya@gmail.com>
2022-08-16 17:53:09 +0200

    renamed config export

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import http from "http";
 import app from "./app";
-import { network_config } from "./utils/config";
+import * as appCnfig from "./utils/config";
 
 const server = http.createServer(app);
 
-const PORT = network_config.PORT || 8080;
+const PORT = appCnfig.network_config.PORT || 8080;
 server.listen(PORT, () => {
     console.table({ PORT });
 });
